2ceceaa24106e0038dc233eb5c5a3d277c7b8888
H M Shahzad <hmshahzad919@gmail.com>
2024-06-13 18:44:25 +0500

    First Commit By EPH

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'user-registration-app';
+  constructor(private _toastr: ToastrService) { }
+
+
+  showSuccess() {
+    this._toastr.success('User Added successfully', 'Success');
+  }
+  showError() {
+    this._toastr.error('User not Added successfully', 'Error');
+  }
+  showInfo() {
+    this._toastr.info('User not Added successfully', 'Info');
+  }
+  showWarning() {
+    this._toastr.warning('User not Added successfully', 'Warning');
+  }
 }
